b0aea583a21642fde4c0d30ce27f39c9b8cebed1
MacPerkins <macdperkins@gmail.com>
2023-02-02 12:06:32 -0600

    Finished scaling up for larger viewports, JS for Mon, Tues Banner

--- assignment_portal/chamber/scripts/scripts.js
+++ assignment_portal/chamber/scripts/scripts.js
@@ -24,3 +24,12 @@ const modif_date = `Last Modified: ${last_modif}`;
 document.querySelector("#last-modified").textContent = modif_date;
 
 document.querySelector("#last-modified").textContent = `Last Modification: ${document.lastModified}`;
+
+// Gets the day of the week. If it is Monday or Tuesday, set the event tab to display as block. Otherwise, leave display as none.
+
+const d = new Date();
+let day = d.getDay();
+
+if (day == 1 && day == 2) {
+    document.querySelector(".tab2").style.display = "block";
+}
