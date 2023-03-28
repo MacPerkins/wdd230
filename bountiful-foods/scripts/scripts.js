d78c51832cda21452879fd84992ceb0580eddae5
MacPerkins <macdperkins@gmail.com>
2023-03-28 15:24:05 -0500

    added content for bountiful foods page

---
+++ bountiful-foods/scripts/scripts.js
@@ -0,0 +1,13 @@
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+let last_modif = new Date(document.lastModified);
+
+const modif_date = `Last Modified: ${last_modif}`;
+document.querySelector("#last-modified").textContent = modif_date;
+
+document.querySelector("#last-modified").textContent = `Last Modification: ${document.lastModified}`;
