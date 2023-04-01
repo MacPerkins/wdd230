f5f779cfb68cbb4282747ebd393d8aeda93a28cb
MacPerkins <macdperkins@gmail.com>
2023-03-31 19:41:02 -0500

    Added small css for index page, images, JS

--- bountiful-foods/scripts/scripts.js
+++ bountiful-foods/scripts/scripts.js
@@ -1,10 +1,3 @@
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
-
 let last_modif = new Date(document.lastModified);
 
 const modif_date = `Last Modified: ${last_modif}`;
